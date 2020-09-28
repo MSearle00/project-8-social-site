2a24bfac115b9cbe20a366afe07851d15ac84fa0
Maria Searle <maria.searle@deeperthanblue.co.uk>
2020-09-28 14:50:47 +0100

    Updates

---
+++ precache-manifest.5717afff4b29de11421e6a9b89b22e61.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c71ddf6b1b25ab341a957168c952284",
+    "url": "/project-8-social-site/index.html"
+  },
+  {
+    "revision": "a8ff86d959e57146dd0f",
+    "url": "/project-8-social-site/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f754bcdae17c3658e976",
+    "url": "/project-8-social-site/static/css/main.c5ed7b5b.chunk.css"
+  },
+  {
+    "revision": "a8ff86d959e57146dd0f",
+    "url": "/project-8-social-site/static/js/2.0a03e30a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project-8-social-site/static/js/2.0a03e30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f754bcdae17c3658e976",
+    "url": "/project-8-social-site/static/js/main.76b54f0f.chunk.js"
+  },
+  {
+    "revision": "b22f7fc1d93457535524",
+    "url": "/project-8-social-site/static/js/runtime-main.7f2f932e.js"
+  },
+  {
+    "revision": "d48003a0dfa1e753e0f694c5519c92c5",
+    "url": "/project-8-social-site/static/media/deletePost.d48003a0.svg"
+  },
+  {
+    "revision": "1d61a5041987cd9ebb5a05128f02581b",
+    "url": "/project-8-social-site/static/media/noNewMessage.1d61a504.svg"
+  },
+  {
+    "revision": "49f7482010fe886119bd77bae9c74a40",
+    "url": "/project-8-social-site/static/media/settings.49f74820.svg"
+  },
+  {
+    "revision": "996d9d2eea30630040f18954f93ed697",
+    "url": "/project-8-social-site/static/media/thumbDown.996d9d2e.svg"
+  },
+  {
+    "revision": "c172ec2ec380d215cfcbf9bc16cbbd1b",
+    "url": "/project-8-social-site/static/media/thumbUp.c172ec2e.svg"
+  }
+]);
